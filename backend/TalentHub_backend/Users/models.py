9eb22b1a8b44ddbe7c6952ac707ce9b02658ca58
Emmanuel <tkemma585@gmail.com>
2026-02-27 16:10:06 +0100

    creation du model User

--- backend/TalentHub_backend/Users/models.py
+++ backend/TalentHub_backend/Users/models.py
@@ -1,22 +1,28 @@
+from django.contrib.auth.models import User
 from django.db import models
+class Skill(models.Model):
+    name = models.CharField(max_length = 20)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    
+    def __str__(self): 
+        return self.name
+    
 
 class Profil(models.Model):
-    bio = models.TextField()
-    skills = models.ForeignKey(Skill, on_delete = models.CASCADE)
-    Profil_picture = models.ImageField(upload_to = "images/")
+    ROLE_CHOICES = (
+        ('client', 'Client'),
+        ('freelancer', 'Freelancer'),
+    )
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    bio = models.TextField(blank = True, null=True)
+    skills = models.ManyToManyField(Skill, blank=True)
+    profil_picture = models.ImageField(upload_to = "images/", blank = True, null=True)
     rating = models.IntegerField()
-    role = models.CharField(max_length = 20)
+    role = models.CharField(max_length = 20 , choices = ROLE_CHOICES)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
     
     def __str__(self):
         return self.role
         
-class Skill(models.Model):
-    name = models.CharField(max_length = 20)
-    create_at = models.DateTimeField(auto_now_add = True)
-    update_at = models.DateTimeField(auto_now = True)
-    
-    def __str__(self): 
-        return self.name
-    
