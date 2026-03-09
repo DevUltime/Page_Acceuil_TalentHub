c1e3fbdd8d41b200d0fb869897519934afacda47
jordan <tioboujordan5@gmail.com>
2026-03-09 17:27:17 +0100

    ajout de la page de connexion

--- Inscription/scriptInscription.js
+++ Inscription/scriptInscription.js
@@ -1,3 +1,4 @@
+// animation du menu hamburger
 function myFunction(){
     let firstBar = document.querySelector(".first_bar"),
     secondBar = document.querySelector(".second_bar"),
@@ -6,8 +7,15 @@ function myFunction(){
     firstBar.classList.toggle("firstBar");
     secondBar.classList.toggle("secondBar");
 };
-
+// appel de la page de connexion
 function getConnexionPage(){
     let connexionPage = document.querySelector(".connexion_page");
     connexionPage.classList.toggle("connexionPage");
 };
+//clic sur le logo de Talenthub
+let logo = document.querySelector("#logo"),
+    a = document.createElement = ("a");
+a.href = "../accueil/index.html";
+logo.addEventListener("click", ()=>{
+    a.click();
+})
--- accueil/script.js
+++ accueil/script.js
@@ -1,3 +1,4 @@
+//animation du menu hamburger
 function myFunction(){
     let firstBar = document.querySelector(".first_bar"),
     secondBar = document.querySelector(".second_bar"),
@@ -9,7 +10,17 @@ function myFunction(){
     secondBar.classList.toggle("secondBar");
     joinUs.classList.toggle('joinUs');
 };
+
+//appel de la page de connexion
 function getConnexionPage(){
     let connexionPage = document.querySelector(".connexion_page");
     connexionPage.classList.toggle("connexionPage");
 };
+
+//clic sur le logo talentHub
+let logo = document.querySelector("#logo"),
+a = document.createElement = ("a");
+a.href = "index.html";
+logo.addEventListener("click", ()=>{
+    a.actived = true
+})
