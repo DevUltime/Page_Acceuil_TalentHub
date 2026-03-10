2b80b4853097b4026a0bfea908363284f88cdb83
jordan <tioboujordan5@gmail.com>
2026-03-10 14:44:29 +0100

    mise a jour de al slide bar

---
+++ Page post inscription/script.js
@@ -0,0 +1,17 @@
+//activation de la side bar
+
+const aside = document.querySelector('aside'),
+btn_fermer_sidebar = document.querySelector(".btn-fermer-sidebar"),
+Profil = document.querySelector('.profil .profil-freelancer');
+
+Profil.addEventListener('click', () => {
+    aside.classList.add('asideActive')
+})
+btn_fermer_sidebar.addEventListener('click', () =>{
+    aside.classList.remove('asideActive')
+})
+document.addEventListener('click', (event) =>{
+    if(!aside.contains(event.target) && !Profil.contains(event.target)) {
+        aside.classList.remove('asideActive')
+    }
+})
